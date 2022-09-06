6a620f1e037b77d15dc667783051391522bc46b4
roberto-ryan <89265506+roberto-ryan@users.noreply.github.com>
2022-09-06 14:43:28 -0400

    Added function Get-vtsTemperature

--- vtsTools.ps1
+++ vtsTools.ps1
@@ -398,3 +398,27 @@ function Install-vtsChromeExtension {
     }
     $result
 }
+
+<#
+.DESCRIPTION
+Returns temperature of thermal sensor on motherboard. Not accurate for CPU temp.
+#>
+function Get-vtsTemperature {
+    $t = Get-WmiObject MSAcpi_ThermalZoneTemperature -Namespace "root/wmi"
+    $returntemp = @()
+
+    foreach ($temp in $t.CurrentTemperature)
+    {
+
+
+    $currentTempKelvin = $temp / 10
+    $currentTempCelsius = $currentTempKelvin - 273.15
+
+    $currentTempFahrenheit = (9/5) * $currentTempCelsius + 32
+
+    $returntemp += $currentTempCelsius.ToString() + " C : " + $currentTempFahrenheit.ToString() + " F : " + $currentTempKelvin + "K"  
+    }
+    return $returntemp
+}
+
+Get-vtsTemperature
