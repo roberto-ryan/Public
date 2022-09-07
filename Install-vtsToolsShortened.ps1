3d3ad15da6840671976ade6a99f35f5bab8fd1dc
roberto-ryan <89265506+roberto-ryan@users.noreply.github.com>
2022-09-07 14:20:47 -0400

    Added TLS1.2 enforcement

--- Install-vtsToolsShortened.ps1
+++ Install-vtsToolsShortened.ps1
@@ -1,1 +1,1 @@
-iex (iwr "https://raw.githubusercontent.com/roberto-ryan/Public/main/Install-vtsTools.ps1" -useb).Content
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; iex (iwr "https://raw.githubusercontent.com/roberto-ryan/Public/main/Install-vtsTools.ps1" -useb).Content
