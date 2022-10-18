34a87751163ed1781c702fee22d07aeb7cd8eaa3
roberto-ryan <89265506+roberto-ryan@users.noreply.github.com>
2022-10-18 13:46:49 -0400

    Added function

--- vtsTools.ps1
+++ vtsTools.ps1
@@ -498,3 +498,39 @@ function Get-vtsTemperature {
     }
     return $returntemp
 }
+
+<#
+.DESCRIPTION
+Returns USB devices and their current status.
+.EXAMPLE
+PS> Get-vtsUSB
+
+Output:
+FriendlyName                                          Present Status
+------------                                          ------- ------
+Microsoft LifeCam VX-3000                                True OK
+EPSON Utility                                            True OK
+American Power Conversion USB UPS                        True OK
+Microsoft LifeCam VX-3000.                               True OK
+FULL HD 1080P Webcam                                     True OK
+SmartSource Pro/Value                                    True OK
+EPSON ES-400                                             True OK
+.EXAMPLE
+PS> Get-vtsUSB epson
+
+Output:
+FriendlyName                                          Present Status
+------------                                          ------- ------
+EPSON Utility                                            True OK
+EPSON ES-400                                             True OK
+#>
+function Get-vtsUSB {
+    param (
+        $searchTerm
+    )
+    
+    get-pnpdevice -friendlyName *$searchTerm* |
+    Where-Object { $_.InstanceId -like "*usb*" } |
+    Select-Object FriendlyName, Present, Status -unique |
+    Sort-Object Present -Descending
+}
