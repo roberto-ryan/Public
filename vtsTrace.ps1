e9b2a3eef5fe0879903b969019d6360e1ba50026
roberto-ryan <robert.ryan@virtechsystems.com>
2023-04-14 18:45:27 -0400

    Updated max tokens

--- vtsTrace.ps1
+++ vtsTrace.ps1
@@ -157,7 +157,7 @@ public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeyst
         $body = @{
             'prompt'            = $prompt;
             'temperature'       = 0;
-            'max_tokens'        = 500;
+            'max_tokens'        = 4000;
             'top_p'             = 1.0;
             'frequency_penalty' = 0.0;
             'presence_penalty'  = 0.0;
