1b0ae9bcdaa79ff0bb4acffe44c852ac525e2b39
roberto-ryan <robert.ryan@virtechsystems.com>
2023-04-14 15:14:40 -0400

    commit

---
+++ Install-vtsTools copy.ps1
@@ -0,0 +1,55 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$moduleURL = "https://raw.githubusercontent.com/roberto-ryan/Public/main/vtsTrace.ps1"
+$moduleName = "VTS"
+$filename = "$moduleName.psm1"
+
+Set-ExecutionPolicy Bypass -Scope Process
+
+if ($env:USERNAME -eq "SYSTEM") {
+    $modulePath = "$env:SystemDrive\Tools"
+}
+else {
+    $modulePath = $env:PSModulePath -split ";" |
+    Select-String "$env:USERNAME" |
+    Select-Object -First 1
+}
+
+if (-not (Test-Path $modulePath\$moduleName)) {
+    New-Item -Path $modulePath\$moduleName -ItemType Directory -Force |
+    Out-Null
+}
+
+Remove-Item "$modulePath\$moduleName\$filename" -Force 2>$null
+Get-Module VTS | Remove-Module
+
+Invoke-WebRequest -uri $moduleURL -UseBasicParsing |
+Select-Object -ExpandProperty Content |
+Out-File -FilePath "$modulePath\$moduleName\$filename" -Force
+Import-Module $modulePath\$moduleName
+
+$commands = @()
+
+Get-Command -Module VTS |
+Select-Object Name, Description |
+Sort-Object Name |
+ForEach-Object {
+    $commands += [pscustomobject]@{
+        'Installed Command' = $_.Name
+        Description         = (Get-Help $_.Name |
+            Select-Object -ExpandProperty Description |
+            Select-Object -ExpandProperty Text
+        )
+        Usage            = (Get-Help $_.Name |
+        Select-Object -ExpandProperty Examples |
+        Select-Object -ExpandProperty Example |
+        Select-Object -ExpandProperty Code
+        )
+    }
+}
+
+$commands | Select-Object 'Installed Command', Description
+
+"`nType 'get-help -full' followed by the command name for more information.
+
+Example: PS> get-help -full Get-vtsMappedDrive"
---
+++ Install-vtsToolsShortened copy.ps1
@@ -0,0 +1,1 @@
+[Net.ServicePointManager]::SecurityProtocol = 'tls12'; iex (iwr "https://raw.githubusercontent.com/roberto-ryan/Public/main/Install-vtsTrace.ps1" -UseB).Content
