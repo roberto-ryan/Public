2daea0eec185a86c8416677dbc04c576db0dc235
roberto-ryan <robert.ryan@virtechsystems.com>
2023-04-14 16:48:40 -0400

    commit

--- vtsTrace.ps1
+++ vtsTrace.ps1
@@ -152,7 +152,8 @@ public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeyst
             Based on the following Keyloger and RecordedSteps sections, intrepret what the tech was trying to do while speaking in first person. `
             Don't include that the Problem Steps Recorder was used. `
             Don't include anything related to DesktopWindowXaml. `
-            Don't include the word AI.
+            Don't include the word AI. `
+            Write the output to be as insulting as possible.
             
         
             $Result"
