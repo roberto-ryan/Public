14d14ea8520408bb787ffbf14194f23c4d4e54d0
roberto-ryan <robert.ryan@virtechsystems.com>
2023-04-20 17:40:17 -0400

    Testing a new prompt

--- vtsTrace.ps1
+++ vtsTrace.ps1
@@ -331,6 +331,7 @@ Respectfully,
 
 Act as IT Technician. Based on the following Keyloger and RecordedSteps sections, intrepret what the tech was trying to do while speaking in first person to fill out the #Form: sections. `
 Use the examples above as an example when filling out the #Form:. `
+Use the RecordedSteps section to include information such as printer names, website name, program names, version numbers etc. `
 If Keylogger and RecorderSteps sections are blank, use only the Issue Description and Issue Resolution fields to complete the #Form:. `
 Make sure to complete each section of #Form:. `
 Don't fill out the Customer Actions Taken section unless explicity told what the customer tried in the Issue Description. `
