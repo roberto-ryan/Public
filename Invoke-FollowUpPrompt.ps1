59682e4b87c29abc5577ec9976df3df9181f65fe
roberto-ryan <robert.ryan@virtechsystems.com>
2023-04-24 10:58:17 -0400

    Testing

--- Invoke-FollowUpPrompt.ps1
+++ Invoke-FollowUpPrompt.ps1
@@ -14,7 +14,8 @@ function GPTFollowUp {
         
     function WriteResultsToHost {
         Clear-Host
-            (Get-Content "$dir\gpt_result2.txt") | ForEach-Object { Write-Host $_ -ForegroundColor Yellow }
+        (Get-Content "$dir\result_header.txt") | ForEach-Object { Write-Host $_ -ForegroundColor Yellow }
+        (Get-Content "$dir\gpt_result2.txt") | ForEach-Object { Write-Host $_ -ForegroundColor Yellow }
     }
     
     While ($true) {
@@ -27,7 +28,7 @@ $(Get-Content $dir\gpt_result.txt -Raw -Encoding utf8)`n`n`n`n Rewrite the IT ti
         $body = @{
             'prompt'            = $prompt;
             'temperature'       = 0;
-            'max_tokens'        = 250;
+            'max_tokens'        = 500;
             'top_p'             = 1.0;
             'frequency_penalty' = 0.0;
             'presence_penalty'  = 0.0;
