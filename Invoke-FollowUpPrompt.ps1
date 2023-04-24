4b8633d651db93db7d53a98fb608b383f1099d96
roberto-ryan <robert.ryan@virtechsystems.com>
2023-04-24 10:10:50 -0400

    testing

--- Invoke-FollowUpPrompt.ps1
+++ Invoke-FollowUpPrompt.ps1
@@ -14,7 +14,7 @@ function GPTFollowUp {
         
     function WriteResultsToHost {
         Clear-Host
-            (Get-Content "$dir\gpt_result.txt") | ForEach-Object { Write-Host $_ -ForegroundColor Yellow }
+            (Get-Content "$dir\gpt_result2.txt") | ForEach-Object { Write-Host $_ -ForegroundColor Yellow }
     }
     
     While ($true) {
@@ -36,7 +36,7 @@ function GPTFollowUp {
         $EncodedJsonBody = [System.Text.Encoding]::UTF8.GetBytes($JsonBody)
 
         $response = Invoke-RestMethod -Uri "https://api.openai.com/v1/engines/text-davinci-003/completions" -Method Post -Body $EncodedJsonBody -Headers @{ Authorization = "Bearer $OpenAIKey" } -ContentType "application/json; charset=utf-8"
-        "$($response.choices.text)" | Out-File "$dir\gpt_result.txt" -Force -Encoding utf8
+        "$($response.choices.text)" | Out-File "$dir\gpt_result2.txt" -Force -Encoding utf8
         WriteResultsToHost
     }
 }
