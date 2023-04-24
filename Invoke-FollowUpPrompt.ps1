11c16d6472a48c7cc2a14a5c465113c8dfa7c639
roberto-ryan <robert.ryan@virtechsystems.com>
2023-04-24 16:46:11 -0400

    Testing

--- Invoke-FollowUpPrompt.ps1
+++ Invoke-FollowUpPrompt.ps1
@@ -25,25 +25,30 @@ $(Get-Content $dir\gpt_result.txt -Raw -Encoding utf8)`n`n`n`n Rewrite the IT ti
     While ($true) {
         
         $alterations = Read-Host "GPT3.5>>>"
-        $prompt = @"
-$(Get-Content $dir\gpt_result2.txt -Raw -Encoding utf8)`n`n`n`n Rewrite the IT ticket above taking into account the following considerations: $alterations.
+        switch ($alterations) {
+            steps { Get-Content $dir\cleaned_steps.txt }
+            Default {
+                $prompt = @"
+        $(Get-Content $dir\gpt_result2.txt -Raw -Encoding utf8)`n`n`n`n Rewrite the IT ticket above taking into account the following considerations: $alterations.
 "@
-    
-        $body = @{
-            'prompt'            = $prompt;
-            'temperature'       = 0;
-            'max_tokens'        = 500;
-            'top_p'             = 1.0;
-            'frequency_penalty' = 0.0;
-            'presence_penalty'  = 0.0;
-            'stop'              = @('"""');
-        }
-        
-        $JsonBody = $body | ConvertTo-Json -Compress
-        $EncodedJsonBody = [System.Text.Encoding]::UTF8.GetBytes($JsonBody)
 
-        $response = Invoke-RestMethod -Uri "https://api.openai.com/v1/engines/text-davinci-003/completions" -Method Post -Body $EncodedJsonBody -Headers @{ Authorization = "Bearer $OpenAIKey" } -ContentType "application/json; charset=utf-8"
-        "$($response.choices.text)" | Out-File "$dir\gpt_result2.txt" -Force -Encoding utf8
-        WriteResultsToHost
+                $body = @{
+                    'prompt'            = $prompt;
+                    'temperature'       = 0;
+                    'max_tokens'        = 500;
+                    'top_p'             = 1.0;
+                    'frequency_penalty' = 0.0;
+                    'presence_penalty'  = 0.0;
+                    'stop'              = @('"""');
+                }
+                
+                $JsonBody = $body | ConvertTo-Json -Compress
+                $EncodedJsonBody = [System.Text.Encoding]::UTF8.GetBytes($JsonBody)
+        
+                $response = Invoke-RestMethod -Uri "https://api.openai.com/v1/engines/text-davinci-003/completions" -Method Post -Body $EncodedJsonBody -Headers @{ Authorization = "Bearer $OpenAIKey" } -ContentType "application/json; charset=utf-8"
+                "$($response.choices.text)" | Out-File "$dir\gpt_result2.txt" -Force -Encoding utf8
+                WriteResultsToHost
+            }
+        }
     }
 }
