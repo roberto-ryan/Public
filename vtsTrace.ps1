3bb05cf65f7e8beb50b0a99ce8f3c1a2bc5e60c6
roberto-ryan <robert.ryan@virtechsystems.com>
2023-06-01 12:03:56 -0400

    Updated Max Tokens

--- vtsTrace.ps1
+++ vtsTrace.ps1
@@ -256,7 +256,7 @@ $(Get-Content "$dir\resolution.txt")
                     "content" = ""
                 })
             "temperature"       = 0
-            'max_tokens'        = 500
+            'max_tokens'        = 2500
             'top_p'             = 1.0
             'frequency_penalty' = 0.0
             'presence_penalty'  = 0.0
