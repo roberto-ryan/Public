b7cd977a3363905bfdf652bb295993b3e52bccfe
roberto-ryan <robert.ryan@virtechsystems.com>
2023-06-05 09:22:30 -0400

    Updated prompt

--- Invoke-FollowUpPrompt.ps1
+++ Invoke-FollowUpPrompt.ps1
@@ -73,7 +73,7 @@ $alterations.
                         },
                         @{
                             "role"    = "system"
-                            "content" = "You always respond in the first person, in the following format:\n\nReported Issue:<text here>\n\nCustomer Actions Taken:<text here>\n\nTroubleshooting Methods:\n- <bulletted troubleshooting steps here>\n\nResolution:<text here>\n\nComments & Misc. info:<text here>\n\nMessage to End User:\n<email to end user here>"
+                            "content" = "You always respond in the first person, in the following format:\n\nReported Issue:<text describing issue\n\nCustomer Actions Taken:<customer actions here\n\nTroubleshooting Methods:\n- <bulletted troubleshooting steps here>\n\nResolution:<resolution here>\n\nComments & Misc. info:<miscellaneous info here>\n\nMessage to End User:\n<email to end user here>"
                         },
                         @{
                             "role"    = "system"
