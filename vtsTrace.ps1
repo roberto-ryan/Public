eff9c76df0e7203852a4ccf3a73c5701685d011a
roberto-ryan <robert.ryan@virtechsystems.com>
2023-08-25 14:23:08 -0400

    Testing custom prompt

--- vtsTrace.ps1
+++ vtsTrace.ps1
@@ -328,7 +328,7 @@ $(Get-Content "$script:dir\resolution.txt")
     try {
         $SessionStart = Timestamp
         DisplayLogo
-        Write-Host "Enter Ticket Description" -ForegroundColor Green
+        Write-Host "Enter Ticket Description" -ForegroundColor Yellow
         $issue = Read-String
 
         if ($issue -ne 'r') {
@@ -404,6 +404,7 @@ $(Get-Content "$script:dir\resolution.txt")
                             'PS ' + $(get-location) + '> '
                         }
                         prompt
+                        exit
                     }
                     $null {
         
