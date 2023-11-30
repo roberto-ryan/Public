6f280ed6031950626c3c238ea1b4df64664904be
roberto-ryan <89265506+roberto-ryan@users.noreply.github.com>
2023-11-30 09:36:52 -0500

    Add Format-vtsMacAddress

--- vtsTools.ps1
+++ vtsTools.ps1
@@ -2172,3 +2172,44 @@ function Get-365UserLicenseDetails {
 
     $LicenseDetails
 }
+
+<#
+.SYNOPSIS
+This function formats a MAC address input by the user.
+
+.DESCRIPTION
+The function Format-vtsMacAddress takes a MAC address as a parameter, removes any separators, converts it to lowercase, checks if it is 12 characters long, and then formats it by inserting colons after every 2 characters. The formatted MAC address is then copied to the clipboard.
+
+.PARAMETER MacAddress
+A string representing the MAC address to be formatted.
+
+.EXAMPLE
+Format-vtsMacAddress -MacAddress "00:0a:95:9d:68:16"
+
+This example formats the provided MAC address.
+#>
+
+function Format-vtsMacAddress {
+    param (
+        [Parameter(Mandatory=$true, HelpMessage="Enter the MAC address.")]
+        $MacAddress
+    )
+
+    # Remove any separators from the MAC address and convert it to lowercase
+    $cleanMac = ($MacAddress -replace '[-:.]', '').ToLower()
+
+    # Check if the MAC address is 12 characters long
+    if ($cleanMac.Length -ne 12) {
+        Write-Host "The MAC address is invalid. It should be 12 hexadecimal characters."
+    } else {
+        # Insert colons after every 2 characters to format the MAC address
+        $outputMac = $cleanMac -replace '(.{2})', '$1:'
+        # Remove the trailing colon
+        $outputMac = $outputMac.TrimEnd(':')
+
+        # Output the formatted MAC address
+        Write-Host "Copied to clipboard:"
+        Write-Host "show mac address-table dynamic address $outputMac"
+        "show mac address-table dynamic address $outputMac" | Set-Clipboard
+    }
+}
