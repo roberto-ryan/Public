f2d20a264ead8be410c995c2ab732576abd45382
roberto-ryan <89265506+roberto-ryan@users.noreply.github.com>
2023-11-30 20:58:21 -0500

    Update Install-vtsTools.ps1

--- Install-vtsTools.ps1
+++ Install-vtsTools.ps1
@@ -32,7 +32,7 @@ $commands = @()
 
 Get-Command -Module VTS |
 Select-Object Name, Description |
-Sort-Object { $_.Name.Split('-')[1] } |
+Sort-Object Name |
 ForEach-Object {
     $commands += [pscustomobject]@{
         'Installed Command' = $_.Name
@@ -40,11 +40,6 @@ ForEach-Object {
             Select-Object -ExpandProperty Description |
             Select-Object -ExpandProperty Text
         )
-        # Usage            = (Get-Help $_.Name |
-        # Select-Object -ExpandProperty Examples |
-        # Select-Object -ExpandProperty Example |
-        # Select-Object -ExpandProperty Code
-        # )
     }
 }
 
