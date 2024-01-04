4b1f04b622b6773428abb8f9352bca5b26dd3f70
Robert Ryan <robert.ryan@virtechsystems.com>
2024-01-04 12:25:22 -0500

    add Get-vtsNICThroughput

--- vtsTools.ps1
+++ vtsTools.ps1
@@ -2548,3 +2548,69 @@ function Search-vtsAllLogs {
   
   Write-Host "Report saved at $ReportPath"
 }
+<#
+.SYNOPSIS
+    Get-vtsNICThroughput is a function that measures the throughput of active network adapters.
+
+.DESCRIPTION
+    This function continuously measures the throughput (in Mbps) of all active network adapters on the system. 
+    It does this by capturing the initial and final statistics of each network adapter over a 2-second interval, 
+    and then calculates the difference in received and sent bytes to determine the throughput.
+
+.PARAMETER adapterName
+    The name of the network adapter. This parameter is mandatory.
+
+.EXAMPLE
+    PS C:\> Get-vtsNICThroughput
+
+    This command will start the continuous measurement of throughput for all active network adapters.
+
+.NOTES
+    To stop the continuous measurement, use Ctrl+C.
+
+.LINK
+    Network
+#>
+function Get-vtsNICThroughput {
+  function CalculateNetworkAdapterThroughput {
+      [CmdletBinding()]
+      param (
+          [Parameter(Mandatory = $true)]
+          [string]$adapterName
+      )
+
+      # Capture initial statistics of the network adapter
+      $statsInitial = Get-NetAdapterStatistics -Name $adapterName
+
+      # Wait for 2 seconds to capture the final statistics
+      Start-Sleep -Seconds 2
+      Clear-Host
+
+      # Capture final statistics of the network adapter
+      $statsFinal = Get-NetAdapterStatistics -Name $adapterName
+
+      # Calculate the differences in received and sent bytes
+      $bytesReceivedDiff = $statsFinal.ReceivedBytes - $statsInitial.ReceivedBytes
+      $bytesSentDiff = $statsFinal.SentBytes - $statsInitial.SentBytes
+
+      # Calculate the throughput in Mbps
+      $throughputInMbps = [Math]::Round($bytesReceivedDiff * 8 / 1MB / 2, 2)
+      $throughputOutMbps = [Math]::Round($bytesSentDiff * 8 / 1MB / 2, 2)
+
+      # Display the throughput
+      Write-Host "Adapter: $adapterName"
+      Write-Host "    Throughput In (Mbps): $throughputInMbps"
+      Write-Host "    Throughput Out (Mbps): $throughputOutMbps"
+  }
+
+  # Infinite loop to continuously measure NIC throughput
+  while ($true) {
+      # Retrieve only the active network adapters
+      $activeAdapters = Get-NetAdapter | Where-Object { $_.Status -eq 'Up' }
+  
+      # Iterate over each active adapter and get throughput
+      foreach ($adapter in $activeAdapters) {
+          CalculateNetworkAdapterThroughput -adapterName $adapter.Name
+      }
+  }
+}
