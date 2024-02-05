9e900276f9b5b74ed0edf1cf0396b85b38b6da8a
Robert Ryan <robert.ryan@virtechsystems.com>
2024-02-05 14:19:35 -0500

    Add Remove-vts365UserLicense

--- vtsTools.ps1
+++ vtsTools.ps1
@@ -2430,6 +2430,42 @@ function Add-vts365UserLicense {
 
 <#
 .SYNOPSIS
+This function removes all licenses from a list of users.
+
+.DESCRIPTION
+The function Remove-vts365UserLicense connects to the Graph API and removes all licenses from a list of users. The user list is passed as a parameter to the function.
+
+.PARAMETER UserList
+A single string or comma separated email addresses from which the licenses are to be removed.
+
+.EXAMPLE
+Remove-vts365UserLicense -UserList "user1@domain.com, user2@domain.com"
+
+.LINK
+M365
+#>
+function Remove-vts365UserLicense {
+  param (
+    [Parameter(Mandatory = $true, HelpMessage = "Enter a single email or a comma separated list of emails.")]
+    $UserList
+  )
+
+  $UserList = ($UserList -split ",").Trim()
+
+  Write-Host "Connecting to Graph API..."
+  Connect-MgGraph -Scopes User.ReadWrite.All, Organization.Read.All
+
+  Write-Host "Removing licenses from users..."
+  foreach ($User in $UserList) {
+    Write-Host "Removing licenses from $User..."
+    $UserLicenses = Get-MgUserLicenseDetail -UserId $User | Select-Object -ExpandProperty SkuId
+    Set-MgUserLicense -UserId $User -AddLicenses @() -RemoveLicenses $UserLicenses
+  }
+  Write-Host "Operation completed."
+}
+
+<#
+.SYNOPSIS
 Searches Windows Event Logs for a specific term and exports the results to a CSV or HTML report.
 
 .DESCRIPTION
