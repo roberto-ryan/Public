158aa8554c6799efc0e43db0ca5a9e91f3daf2dc
roberto-ryan <89265506+roberto-ryan@users.noreply.github.com>
2024-03-18 10:19:25 -0400

    add invoke-systemcheck

--- vtsTools.ps1
+++ vtsTools.ps1
@@ -4581,3 +4581,30 @@ function Revoke-vts365EmailMessage {
 
     Write-Host "Operation completed."
 }
+
+<#
+.SYNOPSIS
+This function invokes a system check using DISM and System File Checker.
+
+.DESCRIPTION
+The Invoke-SystemCheck function initiates a system check by first running the DISM restore health process, followed by a System File Checker scan. It provides status updates at each stage of the process.
+
+.EXAMPLE
+Invoke-SystemCheck
+This command will initiate the system check process.
+
+.NOTES
+The DISM restore health process can help fix Windows corruption errors. The System File Checker scan will scan all protected system files, and replace corrupted files with a cached copy.
+
+.LINK
+Utilities
+#>
+function Invoke-SystemCheck {
+    Write-Host "Starting DISM restore health process..."
+    dism /online /cleanup-image /restorehealth
+    Write-Host "DISM restore health process completed."
+
+    Write-Host "Starting System File Checker scan now..."
+    sfc /scannow
+    Write-Host "System File Checker scan completed."
+}
