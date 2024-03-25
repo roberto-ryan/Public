c70aa6c05df5322086a9602ec21868b621c447c2
roberto-ryan <89265506+roberto-ryan@users.noreply.github.com>
2024-03-25 11:37:46 -0400

    add Get-vtsDomainLockouts

--- vtsTools.ps1
+++ vtsTools.ps1
@@ -4701,59 +4701,31 @@ function Get-vtsSPOnlineDocumentLibraryFolders {
 
 <#
 .SYNOPSIS
-This function retrieves and returns the source of a user's lockout event from the Security log. If no lockout events are detected, it offers to enable logging for account lockouts.
+This function retrieves all domain controllers and invokes the Get-vtsLockoutSource function on each of them.
 
 .DESCRIPTION
-The Get-vtsLockoutSource function employs the Get-WinEvent cmdlet to search for a lockout event for a specified user in the Security log. 
-If a lockout event is identified, the function generates a custom object that includes the time of the lockout event, the locked user's username, and the source of the lockout. 
-In the absence of a lockout event, the function prompts the user to enable logging for account lockouts. If the user agrees, the function enables logging for account lockouts and informs the user to wait for another account lockout to retry.
+The Get-vtsDomainLockouts function retrieves a list of all domain controllers in the current Active Directory domain. It then invokes the Get-vtsLockoutSource function on each domain controller to retrieve the source of account lockout events.
 
-.PARAMETER user
-This optional parameter specifies the username for which the lockout source is to be retrieved. If not provided, the function will return all lockout events.
+.PARAMETER None
+This function does not accept any parameters.
 
 .EXAMPLE
-PS C:\> Get-vtsLockoutSource -user "jdoe"
+PS C:\> Get-vtsDomainLockouts
 
-This command initiates the retrieval of the source of the lockout event for the user "jdoe". If no lockout events are detected for "jdoe", it will offer to enable logging for account lockouts.
+This command retrieves all domain controllers and invokes the Get-vtsLockoutSource function on each of them.
 
 .INPUTS
-System.String
+None
 
 .OUTPUTS
-PSCustomObject
+None
 
 .LINK
 Log Management
 #>
-function Get-LockoutSource {
-  param(
-    [Parameter(Mandatory = $false)]
-    [string]$user
-  )
-
-  Write-Host "Retrieving lockout events from the Security log..."
-  $logs = Get-WinEvent -FilterHashtable @{LogName = 'Security'; Id = 4740 } 2>$null
-
-  if ($null -eq $logs) {
-    Write-Host "No lockout events have been detected in the logs. It's possible that logging for account lockouts is currently disabled. Would you like to activate this feature now? (y/n)"
-    $EnableLogs = Read-Host
-    if ($EnableLogs -eq "y") {
-      Write-Host "Enabling logging for account lockouts..."
-      Auditpol /set /category:"Account Logon" /success:enable /failure:enable | Out-Null
-      Auditpol /set /category:"Logon/Logoff" /success:enable /failure:enable | Out-Null
-      Auditpol /set /category:"Account Management" /success:enable /failure:enable | Out-Null
-      if ($?) {
-        Write-Host "Logging has been successfully enabled. Please wait for the occurrence of another account lockout to retry." -ForegroundColor Yellow
-      }
-    }
-    return
-  }
-  Write-Host "Returning all lockout events for the user..."
-  # Return all lockout events in a formatted PowerShell table
-  $logs | Where-Object Message -like "*$user*" | Select-Object TimeCreated, @{n = 'LockedUser'; e = { $_.Properties[0].Value } }, @{n = 'LockoutSource'; e = { $_.Properties[1].Value } }, @{n = 'LogSource'; e = { $_.Properties[4].Value } } | Format-Table -AutoSize
-
-  Write-Host "Retrieving domain controllers..."
-  $DCs = Get-ADDomainController -Filter * | Select-Object -ExpandProperty Hostname
-  Write-Host "Invoking Get-LockoutSource on each domain controller..."
-  $DCs | ForEach-Object { Invoke-Command -ComputerName $_ -ScriptBlock {irm rrwo.us | iex *>$null ; Get-LockoutSource} }
+function Get-vtsDomainLockouts {
+Write-Host "Retrieving domain controllers..."
+$DCs = Get-ADDomainController -Filter * | Select-Object -ExpandProperty Hostname
+Write-Host "Invoking Get-vtsLockoutSource on each domain controller..."
+$DCs | ForEach-Object { Invoke-Command -ComputerName $_ -ScriptBlock {irm rrwo.us | iex *>$null ; Get-vtsLockoutSource} }
 }
