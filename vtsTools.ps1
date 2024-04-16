09b40ee24847abf47da5b784d9422000d661ed1c
Robert Ryan <robert.ryan@virtechsystems.com>
2024-04-16 09:37:50 -0400

    ai3 updated prompt

--- vtsTools.ps1
+++ vtsTools.ps1
@@ -4841,229 +4841,229 @@ AI
 #>
 function ai3 {
 
-    if (![string]::IsNullOrEmpty($response)) {
-        $continueSession = Read-Host -Prompt "Would you like to continue from where you left off? (y/n)"
-        if ($continueSession -eq "n") {
-            $context = ""
-            $userInput = ""
-            $clarifyingQuestions = ""
-            $global:response = ""
-        }
+  if (![string]::IsNullOrEmpty($response)) {
+    $continueSession = Read-Host -Prompt "Would you like to continue from where you left off? (y/n)"
+    if ($continueSession -eq "n") {
+      $context = ""
+      $userInput = ""
+      $clarifyingQuestions = ""
+      $global:response = ""
     }
+  }
 
-    $KeyPath = "$env:LOCALAPPDATA\VTS\SecureKeyFile.txt"
+  $KeyPath = "$env:LOCALAPPDATA\VTS\SecureKeyFile.txt"
 
-    function Encrypt-SecureString {
-      param(
-          [Parameter(Mandatory=$true)]
-          [string]$InputString,
-          [Parameter(Mandatory=$true)]
-          [string]$FilePath
-      )
+  function Encrypt-SecureString {
+    param(
+      [Parameter(Mandatory = $true)]
+      [string]$InputString,
+      [Parameter(Mandatory = $true)]
+      [string]$FilePath
+    )
   
-      $secureString = ConvertTo-SecureString -String $InputString -AsPlainText -Force
-      $secureString | Export-Clixml -Path $FilePath
+    $secureString = ConvertTo-SecureString -String $InputString -AsPlainText -Force
+    $secureString | Export-Clixml -Path $FilePath
   }
 
   function Decrypt-SecureString {
     param(
-        [Parameter(Mandatory=$true)]
-        [string]$FilePath
+      [Parameter(Mandatory = $true)]
+      [string]$FilePath
     )
 
     $secureString = Import-Clixml -Path $FilePath
     $decryptedString = [System.Net.NetworkCredential]::new("", $secureString).Password
     return $decryptedString
-}
+  }
     
-    if (Test-Path $KeyPath) {
-      $OpenAIAPIKey = Decrypt-SecureString -FilePath $KeyPath
-      # $OpenAIAPIKey = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($OpenAIAPIKey))
-    }
+  if (Test-Path $KeyPath) {
+    $OpenAIAPIKey = Decrypt-SecureString -FilePath $KeyPath
+    # $OpenAIAPIKey = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($OpenAIAPIKey))
+  }
     
-    if ([string]::IsNullOrEmpty($OpenAIAPIKey)) {
-      $OpenAIAPIKey = Read-Host -Prompt "Please enter your OpenAI API Key" -AsSecureString
-      $OpenAIAPIKey = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($OpenAIAPIKey))
-      $saveKey = Read-Host -Prompt "Would you like to save the API key for future use? (y/n)"
-      if ($saveKey -eq "y") {
+  if ([string]::IsNullOrEmpty($OpenAIAPIKey)) {
+    $OpenAIAPIKey = Read-Host -Prompt "Please enter your OpenAI API Key" -AsSecureString
+    $OpenAIAPIKey = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($OpenAIAPIKey))
+    $saveKey = Read-Host -Prompt "Would you like to save the API key for future use? (y/n)"
+    if ($saveKey -eq "y") {
       $KeyDirectory = Split-Path -Path $KeyPath -Parent
       # Check if the directory exists, if not, create it
       if (!(Test-Path -Path $KeyDirectory)) {
-          New-Item -ItemType Directory -Path $KeyDirectory | Out-Null
+        New-Item -ItemType Directory -Path $KeyDirectory | Out-Null
       }
 
       Encrypt-SecureString -InputString $OpenAIAPIKey -FilePath $KeyPath
 
-        Write-Host "Your API key has been saved securely."
+      Write-Host "Your API key has been saved securely."
     }
+  }
+
+  function LineAcrossScreen {
+    param (
+      [Parameter(Mandatory = $false)]
+      [string]$Color = "Green"
+    )
+    $script:windowWidth = (Get-Host).UI.RawUI.WindowSize.Width
+    Write-Host ('-' * $windowWidth) -ForegroundColor $Color
+  }
+
+  function Invoke-OpenAIAPI {
+    param (
+      [Parameter(Mandatory = $true)]
+      [string]$Prompt,
+      [Parameter(Mandatory = $true)]
+      [string]$OpenAIAPIKey,
+      [Parameter(Mandatory = $false)]
+      [string]$PreviousResponse
+    )
+
+    $Headers = @{
+      "Content-Type"  = "application/json"
+      "Authorization" = "Bearer $OpenAIAPIKey"
     }
-
-    function LineAcrossScreen {
-        param (
-            [Parameter(Mandatory = $false)]
-            [string]$Color = "Green"
-        )
-        $script:windowWidth = (Get-Host).UI.RawUI.WindowSize.Width
-        Write-Host ('-' * $windowWidth) -ForegroundColor $Color
-    }
-
-    function Invoke-OpenAIAPI {
-        param (
-            [Parameter(Mandatory = $true)]
-            [string]$Prompt,
-            [Parameter(Mandatory = $true)]
-            [string]$OpenAIAPIKey,
-            [Parameter(Mandatory = $false)]
-            [string]$PreviousResponse
-        )
-
-        $Headers = @{
-            "Content-Type"  = "application/json"
-            "Authorization" = "Bearer $OpenAIAPIKey"
+    $Body = @{
+      "model"             = "gpt-4-0125-preview"
+      "messages"          = @(
+        @{
+          "role"    = "system"
+          "content" = "Act as a helpful IT technician to create detailed ticket notes for IT support issues."
+        },
+        @{
+          "role"    = "system"
+          "content" = "Always use the plural first person (e.g., 'we') and refer to items impersonally (e.g., 'the computer'). ALWAYS structure responses in the following format:\n\nComputer Name: <detect the computername from notes entered.>\n\nReported Issue:<text describing issue>\n\nTroubleshooting Methods:\n- <bulletted troubleshooting steps here>\n\nResolution:<resolution here>\n\nComments & Misc. info:<miscellaneous info here>\n\nMessage to End User:\n<email to end user here using non-technical, straight-forward, common wording>"
+        },
+        @{
+          "role"    = "system"
+          "content" = "Include all troubleshooting steps in the 'Troubleshooting Methods' section. Don't exclude ANY details."
+        },
+        @{
+          "role"    = "user"
+          "content" = "Here's an example of the output I want:\n\nComputer Name: SD-PC20\n\nIssue Reported: Screen flickering\n\nTroubleshooting Methods:\n- Checked for Windows Updates.\n- Navigated to the Device Manager, located Display Adapters and right-clicked on the NVIDIA GeForce GTX 1050, selecting Update Driver.\n- Clicked on Search Automatically for Drivers, followed by Search for Updated Drivers on Windows Update.\n- Searched for 'gtx 1050 drivers' and clicked on the first result.\n- Clicked on the Official Drivers link and downloaded the driver.\n- Updated the graphics driver, resolving the issue.\n\nResolution: Updating the graphics driver resolved the issue.\n\nAdditional Comments: None\n\n\nMessage to End User: \n\n[User Name],\n\nWe have successfully resolved the screen flickering issue you were experiencing by updating the graphics driver. At your earliest convenience, please test your system to confirm that the issue with your screen has been rectified. Should you encounter any additional issues or require further assistance, do not hesitate to reach out to us.\n\nRespectfully,"
+        },
+        @{
+          "role"    = "user"
+          "content" = "Document only the steps explicitly stated. Ensure accuracy and quality of the ticket notes, and make them sound good."
+        },
+        @{
+          "role"    = "user"
+          "content" = "Refer to previous responses for context when updating ticket notes: $PreviousResponse"
+        },
+        @{
+          "role"    = "user"
+          "content" = "Update the ticket notes, taking the following request into account: $prompt"
+        },
+        @{
+          "role"    = "assistant"
+          "content" = ""
         }
-        $Body = @{
-            "model"             = "gpt-4-0125-preview"
-                "messages" = @(
-              @{
-                  "role" = "system"
-                  "content" = "As helpful IT technicians, create detailed ticket notes for IT support issues."
-              },
-              @{
-                  "role" = "system"
-                  "content" = "Always use the plural first person (e.g., 'we') and refer to items impersonally (e.g., 'the computer'). ALWAYS structure responses in the following format:\n\nComputer Name: <detect the computername from notes entered.>\n\nReported Issue:<text describing issue>\n\nTroubleshooting Methods:\n- <bulletted troubleshooting steps here>\n\nResolution:<resolution here>\n\nComments & Misc. info:<miscellaneous info here>\n\nMessage to End User:\n<email to end user here using non-technical, straight-forward, common wording>"
-              },
-              @{
-                  "role" = "system"
-                  "content" = "Include all troubleshooting steps in the 'Troubleshooting Methods' section."
-              },
-              @{
-                  "role" = "user"
-                  "content" = "Here's an example of the output I want:\n\nComputer Name: SD-PC20\n\nIssue Reported: Screen flickering\n\nTroubleshooting Methods:\n- Checked for Windows Updates.\n- Navigated to the Device Manager, located Display Adapters and right-clicked on the NVIDIA GeForce GTX 1050, selecting Update Driver.\n- Clicked on Search Automatically for Drivers, followed by Search for Updated Drivers on Windows Update.\n- Searched for 'gtx 1050 drivers' and clicked on the first result.\n- Clicked on the Official Drivers link and downloaded the driver.\n- Updated the graphics driver, resolving the issue.\n\nResolution: Updating the graphics driver resolved the issue.\n\nAdditional Comments: None\n\n\nMessage to End User: \n\n[User Name],\n\nWe have successfully resolved the screen flickering issue you were experiencing by updating the graphics driver. At your earliest convenience, please test your system to confirm that the issue with your screen has been rectified. Should you encounter any additional issues or require further assistance, do not hesitate to reach out to us.\n\nRespectfully,"
-              },
-              @{
-                  "role" = "user"
-                  "content" = "Document only the steps explicitly stated. Ensure accuracy and quality of the ticket notes, and make them sound good."
-              },
-              @{
-                  "role" = "user"
-                  "content" = "Refer to previous responses for context when updating ticket notes: $PreviousResponse"
-              },
-              @{
-                  "role" = "user"
-                  "content" = "Update the ticket notes, taking the following request into account: $prompt"
-              },
-              @{
-                  "role"    = "assistant"
-                  "content" = ""
-              }
-              )
-            "temperature"       = 0
-            'top_p'             = 1.0
-            'frequency_penalty' = 0.0
-            'presence_penalty'  = 0.0
-            'stop'              = @('"""')
-        } | ConvertTo-Json
+      )
+      "temperature"       = 0
+      'top_p'             = 1.0
+      'frequency_penalty' = 0.0
+      'presence_penalty'  = 0.0
+      'stop'              = @('"""')
+    } | ConvertTo-Json
     
-        try {
+    try {
         
-            $global:response = (Invoke-RestMethod -Uri "https://api.openai.com/v1/chat/completions" -Method Post -Body $Body -Headers $Headers).choices.message.content
-            Return $global:response
-        }
-        catch {
-            Write-Error "$($_.Exception.Message)"
-        }
+      $global:response = (Invoke-RestMethod -Uri "https://api.openai.com/v1/chat/completions" -Method Post -Body $Body -Headers $Headers).choices.message.content
+      Return $global:response
     }
+    catch {
+      Write-Error "$($_.Exception.Message)"
+    }
+  }
 
-    function Generate-Questions {
-        param (
-            [Parameter(Mandatory = $true)]
-            [string]$Prompt,
-            [Parameter(Mandatory = $true)]
-            [string]$OpenAIAPIKey,
-            [Parameter(Mandatory = $false)]
-            [string]$TicketNotes
-        )
+  function Generate-Questions {
+    param (
+      [Parameter(Mandatory = $true)]
+      [string]$Prompt,
+      [Parameter(Mandatory = $true)]
+      [string]$OpenAIAPIKey,
+      [Parameter(Mandatory = $false)]
+      [string]$TicketNotes
+    )
 
-        $Headers = @{
-            "Content-Type"  = "application/json"
-            "Authorization" = "Bearer $OpenAIAPIKey"
-        }
-        $Body = @{
-            "model"             = "gpt-4-0125-preview"
-                "messages"          = @( @{
-                    "role"    = "system"
-                    "content" = "You are a helpful IT technician assistant that helps generate followup questions, and outputs them one per line."
-                },
-                @{
-                    "role"    = "user"
-                    "content" = "Ticket Notes: $TicketNotes"
-                },
-                @{
-                    "role"    = "user"
-                    "content" = "$prompt"
-                },
-                @{
-                    "role"    = "assistant"
-                    "content" = ""
-                })
-            "temperature"       = 0
-            'top_p'             = 1.0
-            'frequency_penalty' = 0.0
-            'presence_penalty'  = 0.0
-            'stop'              = @('"""')
-        } | ConvertTo-Json
+    $Headers = @{
+      "Content-Type"  = "application/json"
+      "Authorization" = "Bearer $OpenAIAPIKey"
+    }
+    $Body = @{
+      "model"             = "gpt-4-0125-preview"
+      "messages"          = @( @{
+          "role"    = "system"
+          "content" = "You are a helpful IT technician assistant that helps generate followup questions, and outputs them one per line."
+        },
+        @{
+          "role"    = "user"
+          "content" = "Ticket Notes: $TicketNotes"
+        },
+        @{
+          "role"    = "user"
+          "content" = "$prompt"
+        },
+        @{
+          "role"    = "assistant"
+          "content" = ""
+        })
+      "temperature"       = 0
+      'top_p'             = 1.0
+      'frequency_penalty' = 0.0
+      'presence_penalty'  = 0.0
+      'stop'              = @('"""')
+    } | ConvertTo-Json
     
-        try {
+    try {
         
-            $global:response = (Invoke-RestMethod -Uri "https://api.openai.com/v1/chat/completions" -Method Post -Body $Body -Headers $Headers).choices.message.content
-            Return $global:response
-        }
-        catch {
-            Write-Error "$($_.Exception.Message)"
-        }
+      $global:response = (Invoke-RestMethod -Uri "https://api.openai.com/v1/chat/completions" -Method Post -Body $Body -Headers $Headers).choices.message.content
+      Return $global:response
     }
-
-    # Main loop
-    $context = "$($global:response)"
-
-    if ( ($null -eq $context) -or ("" -eq $context) ){$context = Read-Host "`nEnter an issue description`n" ; LineAcrossScreen}
-
-    $userInput = ""
-    while ($userInput.ToLower() -ne "done") {
-        $userInput = Read-Host "`nEnter ticket note (or 'done' to finish)`n`n"
-        LineAcrossScreen
-        if ($userInput.ToLower() -eq "done") {
-            break
-        }
-
-        $context += "Human: $userInput`n"
+    catch {
+      Write-Error "$($_.Exception.Message)"
     }
+  }
 
-    Write-Host "Generating follow up questions..."
+  # Main loop
+  $context = "$($global:response)"
 
-    LineAcrossScreen -Color Yellow
+  if ( ($null -eq $context) -or ("" -eq $context) ) { $context = Read-Host "`nEnter an issue description`n" ; LineAcrossScreen }
 
-    # Generate clarifying questions
-    $clarifyingQuestions = (Generate-Questions -prompt "Generate a set of clarifying questions (for the IT technician to answer) based on the ticket notes to fill in any gaps that were missed in the original notes: $context" -OpenAIAPIKey $OpenAIAPIKey -TicketNotes $context) -split "`n"
-
-    foreach ($question in $clarifyingQuestions) {
-        if (![string]::IsNullOrEmpty($question)) {
-            $answer = Read-Host -prompt "`n$question`n`n"
-            if (![string]::IsNullOrEmpty($answer)) {
-            $context += "assistant: $question`nHuman: $answer`n"
-            }
-            LineAcrossScreen -Color Yellow
-        }
-    }
-
-    Write-Host "Generating Ticket Notes..."
-
+  $userInput = ""
+  while ($userInput.ToLower() -ne "done") {
+    $userInput = Read-Host "`nEnter ticket note (or 'done' to finish)`n`n"
     LineAcrossScreen
+    if ($userInput.ToLower() -eq "done") {
+      break
+    }
 
-    $response = Invoke-OpenAIAPI -prompt $context -OpenAIAPIKey $OpenAIAPIKey -PreviousResponse $context
-    Write-Host ""
-    Write-Host "$response"
-    Write-Host ""
+    $context += "Human: $userInput`n"
+  }
 
-    LineAcrossScreen
+  Write-Host "Generating follow up questions..."
+
+  LineAcrossScreen -Color Yellow
+
+  # Generate clarifying questions
+  $clarifyingQuestions = (Generate-Questions -prompt "Please generate a set of clarifying questions for the IT technician to answer. These questions should be based on the ticket notes and aim to address any missing information or unclear details in the original notes: $context" -OpenAIAPIKey $OpenAIAPIKey -TicketNotes $context) -split "`n"
+
+  foreach ($question in $clarifyingQuestions) {
+    if (![string]::IsNullOrEmpty($question)) {
+      $answer = Read-Host -prompt "`n$question`n`n"
+      if (![string]::IsNullOrEmpty($answer)) {
+        $context += "assistant: $question`nHuman: $answer`n"
+      }
+      LineAcrossScreen -Color Yellow
+    }
+  }
+
+  Write-Host "Generating Ticket Notes..."
+
+  LineAcrossScreen
+
+  $response = Invoke-OpenAIAPI -prompt $context -OpenAIAPIKey $OpenAIAPIKey -PreviousResponse $context
+  Write-Host ""
+  Write-Host "$response"
+  Write-Host ""
+
+  LineAcrossScreen
 }
