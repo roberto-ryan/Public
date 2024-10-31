2a5b006e22eaded59a4cb776df6ee27c1c8cb245
roberto-ryan <89265506+roberto-ryan@users.noreply.github.com>
2024-10-30 20:31:58 -0400

    Update Install-vtsPwsh
    Added $InstallLatestVersionWithGUI switch to which always installs the latest version of PowerShell, but requires GUI interaction.

--- vtsTools.ps1
+++ vtsTools.ps1
@@ -1141,10 +1141,18 @@ PS> Install-vtsPwsh
 Package Management
 #>
 function Install-vtsPwsh {
-  msiexec.exe /i "https://github.com/PowerShell/PowerShell/releases/download/v7.4.3/PowerShell-7.4.3-win-x64.msi" /qn
-  Write-Host "Installing PowerShell 7... Please wait" -ForegroundColor Cyan
-  While (-not (Test-Path "C:\Program Files\PowerShell\7\pwsh.exe" 2>$null)) { Start-Sleep 5 }
-  & "C:\Program Files\PowerShell\7\pwsh.exe" 2>$null
+  param (
+    [switch]$InstallLatestVersionWithGUI
+  )
+
+  if ($InstallLatestVersionWithGUI){
+    iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI"
+  } else {
+    msiexec.exe /i "https://github.com/PowerShell/PowerShell/releases/download/v7.4.6/PowerShell-7.4.6-win-x64.msi" /qn
+    Write-Host "Installing PowerShell 7... Please wait" -ForegroundColor Cyan
+    While (-not (Test-Path "C:\Program Files\PowerShell\7\pwsh.exe" 2>$null)) { Start-Sleep 5 }
+    & "C:\Program Files\PowerShell\7\pwsh.exe" 2>$null
+  }
 }
 
 <#
